149cc43cfddb027e2d2160803e50464adcc9abc1
Kyle Henderson <kthenderson83@gmail.com>
2019-06-18 11:38:01 -0500

    finish 3

--- joins.solution.sql
+++ joins.solution.sql
@@ -11,3 +11,11 @@ SELECT "orders"."id", "orders"."order_date", "quantity", "description"
 FROM "orders"
 LEFT JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
 LEFT JOIN "products" ON "line_items"."product_id" = "products"."id";
+
+-- 	Which warehouses have cheetos?
+
+SELECT *
+FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
+JOIN "products" ON "warehouse_product"."product_id" = "products"."id"
+WHERE "products"."description" = 'cheetos';
